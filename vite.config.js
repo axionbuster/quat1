bb09a97e8252e8c60d0eebd27ee7e78a591fb9fb
axionbuster <axionbuster@gmail.com>
2024-05-23 15:31:51 -0700

    Use proper repo name

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 /** @type {import('vite').UserConfig} */
 export default {
-    // GitHub pages under https://<USERNAME>.github.io/<REPO>/, where <REPO> = "quatcalc"
-    base: '/quatcalc/'
+    // GitHub pages under https://<USERNAME>.github.io/<REPO>/, where <REPO> = "quat1"
+    base: '/quat1/'
 }
